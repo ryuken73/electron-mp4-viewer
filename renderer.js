aa04068cf2b7d234c929cf5df65ca442a7927365
류건우집 <ryuken73@naver.com>
2018-06-10 22:55:46 +0900

    at home

--- renderer.js
+++ renderer.js
@@ -933,7 +933,17 @@ function insertRows(serverConfigs){
         logger.info('save success! : %j', server);       
     })
 }
+const {Menu, MenuItem} = remote
 
+const menu = new Menu()
+menu.append(new MenuItem({label: 'MenuItem1', click() { console.log('item 1 clicked') }}))
+menu.append(new MenuItem({type: 'separator'}))
+menu.append(new MenuItem({label: 'MenuItem2', type: 'checkbox', checked: true}))
+
+window.addEventListener('contextmenu', (e) => {
+  e.preventDefault()
+  menu.popup({window: remote.getCurrentWindow()})
+}, false)
 /*
 // make modal windows
 // too slow
